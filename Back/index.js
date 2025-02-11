e590503383eedf8ba27d156464c2abd4fed338a1
Damien <damienpizarro94@gmail.com>
2025-02-11 14:26:59 +0100

    bug

--- Back/index.js
+++ Back/index.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../front")));
+app.use(express.static(path.join(__dirname, "../Front")));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../front/index.html"));
+    res.sendFile(path.join(__dirname, "../Front/index.html"));
 });
 
 const PORT = 4000;
